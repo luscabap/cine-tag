a9ed6e6e2563f61a042e7f8c56bcd91803f551ad
luscabap <lucasbaptistasilva.dev@gmail.com>
2023-12-29 15:31:27 -0300

    CineTag

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Favoritos from "./pages/Favoritos";
+import Inicio from "./pages/Inicio";
+import NaoEncontrada from "./pages/NaoEncontrada";
+import PaginaBase from "./pages/PaginaBase";
+import Player from "./pages/Player";
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />}></Route>
+                    <Route path="favoritos" element={<Favoritos />}></Route>
+                    <Route path=":id" element={<Player />}></Route>
+                    <Route path="*" element={<NaoEncontrada />}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes;
